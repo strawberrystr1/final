cc3d7b008dea221f33801a1fac5706734fa5cdb4
strawberrystr1 <andrey_overdose@mail.ru>
2023-03-02 20:22:07 +0300

    fix: fix db connection

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,17 +1,17 @@
 import { Sequelize } from "sequelize";
 
-const dbClient = new Sequelize("collector", "postgres", "password", {
-  host: "localhost",
-  port: 5432,
-  dialect: "postgres",
-  logging: false
-});
+// const dbClient = new Sequelize("collector", "postgres", "password", {
+//   host: "localhost",
+//   port: 5432,
+//   dialect: "postgres",
+//   logging: false
+// });
 
-// const dbClient = new Sequelize(
-//   "postgres://collector:Ww6TFXRTIDT289cBujY0yw4VpVDwYsGV@dpg-cfvhsrndvk4rro62co1g-a/collector",
-//   {
-//     logging: false
-//   }
-// );
+const dbClient = new Sequelize(
+  "postgres://collector:Ww6TFXRTIDT289cBujY0yw4VpVDwYsGV@dpg-cfvhsrndvk4rro62co1g-a/collector",
+  {
+    logging: false
+  }
+);
 
 export default dbClient;
